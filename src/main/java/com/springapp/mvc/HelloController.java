8ac21a1bc9ce20c9e9ec963c4f2248551978aa0e
Daniel Melton <daniel.melton@capitalone.com>
2016-08-14 15:03:19 -0400

    initialized project, added spring and hibernate dependencies

---
+++ src/main/java/com/springapp/mvc/HelloController.java
@@ -0,0 +1,18 @@
+package com.springapp.mvc;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+	@RequestMapping(method = RequestMethod.GET)
+	public String printWelcome(ModelMap model) {
+		model.addAttribute("message", "Hello world!");
+		return "hello";
+	}
+}
