97ae3671c5bd2fd5e60adff9972cf39d33f30fc9
Daniel Melton <daniel.melton@capitalone.com>
2016-08-15 10:58:34 -0400

    added wrapper class for User and Questions into one wrapper so @RequestBody can look at the entire request at once

--- src/main/java/com/springapp/mvc/controllers/APIController.java
+++ src/main/java/com/springapp/mvc/controllers/APIController.java
@@ -1,14 +1,10 @@
 package com.springapp.mvc.controllers;
 
 
-import com.springapp.mvc.model.Questions;
-import com.springapp.mvc.model.QuestionsWrapper;
-import com.springapp.mvc.model.User;
+import com.springapp.mvc.model.ContentWrapper;
 import com.springapp.mvc.service.QuestionsService;
 import com.springapp.mvc.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 //this is a rest api that needs to accept json input which will have name, id of question, and the answer. Need
@@ -24,11 +20,10 @@ public class APIController {
 	private QuestionsService questionsService;
 
 	@RequestMapping(method = RequestMethod.POST, consumes = "application/json")
-	public void consumeApplication(@RequestBody User user, @RequestBody QuestionsWrapper questions) {
-		userService.saveUser(user);
-
-		for(int i = 0; i < questions.getQuestions().size(); i++) {
-			questionsService.saveQuestions(questions.getQuestions().get(i));
+	public void consumeApplication(@RequestBody ContentWrapper content) {
+		userService.saveUser(content.getUser());
+		for(int i = 0; i < content.getQuestions().size(); i++) {
+			questionsService.saveQuestions(content.getQuestions().get(i));
 		}
 	}
 
--- src/main/java/com/springapp/mvc/model/ContentWrapper.java
+++ src/main/java/com/springapp/mvc/model/ContentWrapper.java
@@ -5,9 +5,19 @@ import java.util.List;
 /**
  * Created by rhy704 on 8/15/16.
  */
-public class QuestionsWrapper {
+public class ContentWrapper {
+
+    private User user;
 
     private List<Questions> questions;
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
 
     public List<Questions> getQuestions() {
         return questions;
